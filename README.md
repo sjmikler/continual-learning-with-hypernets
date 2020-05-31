92cbc7c270689ed58b0541938ac1150403580b53
Szymon Mikler <sjmikler@gmail.com>
2020-05-31 23:43:12 +0200

    Now there's testing on task 0 to see the need of regularization

--- README.md
+++ README.md
@@ -5,32 +5,27 @@ The intention is to run experiments using `.yaml` config file as in `experiment.
 `experiments.yaml` is copied from another project of mine, it's not ready to use here.
 
 *Tasks:*
-* Short term
+* PermutedMNIST-100
+    * ✔ Create meta model, that generates models, as in (1)
     * ✔ Make hypernetworks learn **chunks** of smaller network, not all weights at once
     * ✔ Inject hypernetwork from playground to separate model, i.e. "Lenet Hypernet"
     * ✔ Reproduce hypernetwork result on separete task (over 90% accuracy)
-    * Create `train.py` file compatibile with `models` and `datasets`
-    * Test HyperNetwork in continual learning scenario
-    
-    
-* PermutedMNIST-100
-    * Prepare models to compare with, i.e. models learning all tasks at once
-    * ✔ Create meta model, that generates models, as in [1]
     * ✔ Make it learn anything in continual learning scenario and generate tokens for tasks
-    * Create evaluations that **know** what task to do now!
-    * Create evaluations that have to infer, what task to do now!
-    * Make sure it matches results from [1]
-    
-    
-* Split MNIST (read [1], [2]) 
+    * ✔ Create evaluations that cheats and **knows** what task to do now!
+    * ✔ Test HyperNetwork in continual learning scenario
+    * Use regularization to finally stop the forgetting
+    * Prepare models we want to compare with, models that have access to all tasks at once
+    * Create `train.py` file compatibile with `models` and `datasets`
+    * Create evaluations that have to infer, what task they are predicting!
+    * Make sure network matches results from (1)
+
+* Split MNIST (read (1), (2))
     * Create dataset
-    
-    
+
 * Split CIFAR-10
     * Prepare ResNet model and make it learn CIFAR-10
-    
-    
+
 **This repository reproduces or uses ideas from following papers:**
-* [1] Continual learning with hypernetworks, https://arxiv.org/abs/1906.00695
-* [2] Three scenarios for continual learning, https://arxiv.org/pdf/1904.07734.pdf
-* [3] HyperNetworks, https://arxiv.org/pdf/1609.09106.pdf
+* (1) Continual learning with hypernetworks, <https://arxiv.org/abs/1906.00695>
+* (2) Three scenarios for continual learning, <https://arxiv.org/pdf/1904.07734.pdf>
+* (3) HyperNetworks, <https://arxiv.org/pdf/1609.09106.pdf>
--- experiment1.py
+++ experiment1.py
@@ -1,9 +1,14 @@
+import random
+
 import tensorflow as tf
+
 from datasets import mnist
 from models import hnet_with_chunks
 from training_utils import get_training_function
 
 # TODO: THESE ARE CONSTANTS TO BE READ FROM experiments.yaml:
+RUN_NAME = 'multitask_testing'
+LOGDIR = 'logs'
 BATCH_SIZE = 128
 EMBEDDING_DIM = 50
 N_CHUNKS = 40
@@ -18,30 +23,50 @@ model = hnet_with_chunks.create(embedding_dim=EMBEDDING_DIM,
                                 inner_net_dims=INNER_NET_DIMS,
                                 l2reg=L2REG)
 
+rnd_idx = random.randint(1000, 9999)
+writer = tf.summary.create_file_writer(f'{LOGDIR}/{RUN_NAME}{rnd_idx}')
+
 
 def train_on_task(model, task_idx, iterations):
     """
     Trains the model in PermutedMNIST subtask and generates the token for task
     :param model: `tf.keras.Model` instance
-    :param task_idx: an integer from 0 to 99
-    :param iterations: number of batches to proceed
+    :param task_idx: an integer, for Permuted MNIST from 0 to 99
+    :param iterations: number of batches to train the task from
     :return: learned embedding of the task with index `task_idx`
     """
     task_embedding = tf.random.normal([EMBEDDING_DIM]) / 10
     task_embedding = tf.Variable(task_embedding, trainable=True)
-    
+
     train_ds = task_generator.get_iterator(split='train', task=task_idx)
     test_ds = task_generator.get_iterator(split='test', task=task_idx)
-    
+
     loss_fn = tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True)
     optimizer = tf.keras.optimizers.Adam()
-    
+
     train_epoch = get_training_function(model, loss_fn, optimizer)
     train_epoch(token=task_embedding,
                 steps_per_epoch=iterations,
                 train_ds=train_ds,
                 test_ds=test_ds)
     return task_embedding
+
+
+def test_on_task(model, task_idx, task_embedding):
+    """
+    :param model: `tf.keras.Model` instance
+    :param task_idx: an integer, for Permuted MNIST from 0 to 99
+    :param task_embedding: float vector of embeddings for task, with dimensionality
+        100 for first lenet
+    :return: accuracy float between 0.0 and 1.0
+    """
+    test_ds = task_generator.get_iterator(split='test', task=task_idx)
+    accuracy = tf.metrics.SparseCategoricalAccuracy()
+
+    for x, y in test_ds:
+        outs, _ = model([task_embedding, x], training=False)
+        accuracy.update_state(y, outs)
+    return accuracy.result()
 
 
 task_data = {}
@@ -54,4 +79,8 @@ for task_idx in range(10):
     _, inner_net_weights = model([embed, tf.random.uniform([1, 784])])
     task_data[task_idx] = (embed, inner_net_weights)
 
-# TODO: proper testing on multiple tasks, not only current
+    acc_on_0 = test_on_task(model, task_idx=0, task_embedding=task_data[0][0])
+    with writer.as_default():
+        tf.summary.scalar(name='task0_acc', data=acc_on_0, step=task_idx)
+
+    # TODO: add old task regularization to stop catastrophic forgetting
